61682f67a567559277a32513f1d6d299a89aedc9
zhangjincheng <zhangjincheng@huidr.com>
2019-05-08 17:12:55 +0800

    Security

---
+++ src/Security/GetSecurity.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: administrato
+ * Date: 2019/5/8
+ * Time: 17:06
+ */
+
+namespace GoSwoole\Plugins\Security;
+
+use GoSwoole\Plugins\Security\Beans\Principal;
+use GoSwoole\Plugins\Session\GetSession;
+
+trait GetSecurity
+{
+    use GetSession;
+
+    public function getPrincipal(): ?Principal
+    {
+        return $this->getSession()->getAttribute("Principal");
+    }
+
+    public function setPrincipal(Principal $principal)
+    {
+        $this->getSession()->setAttribute("Principal", $principal);
+    }
+}
